a8baccb083b46847883e1bcfd757da64b3f54559
Hans Lo <hansshulo@gmail.com>
2011-06-16 01:49:34 -0400

    Touhou Names module.
    Named constants defined here.

---
+++ touhou/touhou_names.py
@@ -0,0 +1,30 @@
+'''
+* This file is part of Touhou SRPG.
+* Copyright (c) Hans Lo
+*
+* Touhou SRPG is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Touhou SRPG is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Touhou SRPG.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+from pygame.locals import *
+
+#User Event defines
+UI_EVENT = USEREVENT+3
+OBJECTEVENT = USEREVENT+4
+
+#Interface modes
+I_BROWSE, I_MOVE, I_ATTACK = range(3)
+
+# UI Event subtypes
+MOVETO, ATTACK, ENDTURN = range(3)
+
